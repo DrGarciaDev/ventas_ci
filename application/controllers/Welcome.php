2f40c43465357f755e44c8819f27749200aca7d8
Luis García <luis_grdz@hotmail.com>
2019-06-29 23:24:11 -0500

    Integración del proyecto descargado tal cual del autor del video tutorial

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('layouts/header');
+		$this->load->view('layouts/aside');
+		$this->load->view('admin/dashboard');
+		$this->load->view('layouts/footer');
+
 	}
 }
